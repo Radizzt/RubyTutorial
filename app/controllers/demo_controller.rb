679a76224f32ea2a058f8584e480b64d1eabb089
Radizzt <Richardthienhuynh@gmail.com>
2016-06-30 15:01:15 -0400

    Database finished.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,11 +3,23 @@ class DemoController < ApplicationController
   layout false;
 
   def index
-    render(:template => 'demo/hello');
+    #render(:template => 'demo/hello');
   end
 
   def hello
-    render('demo/index');
+    #render('demo/index');
+    @array = [1,2,3,4,5];
+    #doensnt matter if its string '' or symbol :
+    @id = params['id'];
+    #convert @page to integer
+    @page = params[:page].to_i;
   end
 
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index');
+  end
+
+  def lynda
+    redirect_to("http://lynda.com");
+  end
 end
